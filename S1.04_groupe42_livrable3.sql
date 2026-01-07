f0a262805e92318ebad7d8835669a68b5f057bf1
ignir64 <thibaut210107@gmail.com>
2026-01-07 17:50:10 +0100

    Update SQL queries for ordering and add comments
    Modified the first query to order by e.CODE in descending order and added comments for clarity.

--- S1.04_groupe42_livrable3.sql
+++ S1.04_groupe42_livrable3.sql
@@ -1,8 +1,10 @@
 SELECT h.CODE, h.NOMPROP, h.CODEEPI, e.CODE, e.DESCRIPTION
 FROM HEBERGEMENT 
 INNER JOIN EPI ON h.CODEEPI = e.CODE
-ORDER BY e.CODE;
+ORDER BY e.CODE DESC;
+-- Permet de trier les hebergement de manière décroissant en fonction du nombre d'épi.
 
 SELECT CODE, TARIFSBASECHAMBRE AS TARIF, TARIFBASELITSUP,CAPACITE
 FROM HEBERGEMENT
 ORDER BY TARIF ASC;
+-- Permet de trier le prix des hebergement en fonoction du tarifs des chambres de manière croissante.
