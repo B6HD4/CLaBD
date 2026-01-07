452ad20ded2141289ef9eec01d04d91a5ad7633a
ignir64 <thibaut210107@gmail.com>
2026-01-07 17:54:40 +0100

    Enhance SQL queries with descriptive comments
    Added comments to clarify the purpose of each SQL query.

--- S1.04_groupe42_livrable3.sql
+++ S1.04_groupe42_livrable3.sql
@@ -2,9 +2,13 @@ SELECT h.CODE, h.NOMPROP, h.CODEEPI, e.CODE, e.DESCRIPTION
 FROM HEBERGEMENT 
 INNER JOIN EPI ON h.CODEEPI = e.CODE
 ORDER BY e.CODE DESC;
--- Permet de trier les hebergement de manière décroissant en fonction du nombre d'épi.
+-- Destinataire : Un client
+-- Interêt : Permet de trier les hébergements de manière décroissante en fonction du nombre d'épis.
+
 
 SELECT CODE, TARIFSBASECHAMBRE AS TARIF, TARIFBASELITSUP,CAPACITE
 FROM HEBERGEMENT
 ORDER BY TARIF ASC;
--- Permet de trier le prix des hebergement en fonoction du tarifs des chambres de manière croissante.
+-- Destinataire : Un client
+-- Interêt : Permet de trier le prix des hébergements en fonction du tarifs des chambres de manière croissante.
+
