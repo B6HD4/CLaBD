f51ccccf490c15aa2ee08d3f5424403032793507
Ced <crouille@iutbayonne.univ-pau.fr>
2026-01-07 09:09:23 +0100

    add consignes

---
+++ S1.04_groupe42_livrable3.sql
@@ -0,0 +1,11 @@
+-- 2 jointures
+
+-- 2 ORDER BY dont au moins 1 mettant en œuvre plusieurs attributs de tri
+
+-- 3 GROUP BY (criteres statiques)
+
+-- 3 GROUP BY HAVING dont 1 avec sous-requête dans le HAVING (critere dynamique)
+
+-- Fonctions d’agrégation : 2 requêtes avec une ou plusieurs fonctions d’agrégation (MIN, MAX, etc.)
+
+-- Sous-requêtes : 4 requêtes utilisant au niveau de l’imbrication des opérateurs de comparaison ainsi que les prédicats IN et NOT IN
