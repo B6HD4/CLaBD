740b7b06444dcf85fd677f823f2f415b63345460
ignir64 <thibaut210107@gmail.com>
2026-01-07 18:24:32 +0100

    Add SQL queries for accommodation sorting
    Added multiple SELECT queries for sorting accommodations based on various criteria.

--- S1.04_groupe42_livrable3.sql
+++ S1.04_groupe42_livrable3.sql
@@ -12,3 +12,19 @@ ORDER BY TARIF ASC;
 -- Destinataire : Un client
 -- Interêt : Permet de trier le prix des hébergements en fonction du tarifs des chambres de manière croissante.
 
+SELECT CODE, NOMPROP, CAPACITE, TARIFBASECHAMBRE, CODECOMMUNE
+FROM HEBERGEMENT 
+WHERE CAPACITE < 4
+GROUP BY CODECOMMUNE
+HAVING TARIFBASECHAMBRE < 150;
+-- Destinataire : Un client
+-- Interêt : Récupère les logement trier pour un budget limité et un nombre de personne minimale.
+
+SELECT CODE, CODEREGION, CODEEPI
+FROM HEBERGEMENT
+WHERE CODEEPI > 1
+GROUP BY CODEREGION NOT IN(SELECT h.code, h.TARIFCHAMBREBASE
+                           FROM HEBERGEMENT h
+                           WHERE TARIFCHAMBREBASE < 200);
+-- Destinataire : Un client
+-- Interêt : Tri un hébergement en fonction de plusieur critère sélectionner, l'épi doit être supérieur à 1, le prix inférieur à 200 et trier par région. 
