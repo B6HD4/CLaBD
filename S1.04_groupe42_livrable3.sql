c0d666fc413b58800f0464e867d8db1a95d9bf6f
Ced <crouille@iutbayonne.univ-pau.fr>
2026-01-09 17:07:58 +0100

    rectification des deux premieres requêtes

--- S1.04_groupe42_livrable3.sql
+++ S1.04_groupe42_livrable3.sql
@@ -1,31 +1,33 @@
 SELECT h.CODE, h.NOMPROP, h.CODEEPI, e.CODE, e.DESCRIPTION
-FROM HEBERGEMENT 
-INNER JOIN EPI ON h.CODEEPI = e.CODE
+FROM HEBERGEMENT h
+INNER JOIN EPI e ON h.CODEEPI = e.CODE
 ORDER BY e.CODE DESC;
 -- Destinataire : Un client
 -- Interêt : Permet de trier les hébergements de manière décroissante en fonction du nombre d'épis.
 
 
-SELECT CODE, TARIFSBASECHAMBRE AS TARIF, TARIFBASELITSUP,CAPACITE
+SELECT CODE, TARIFBASECHAMBRE AS "TARIF", TARIFBASELITSUP,CAPACITE
 FROM HEBERGEMENT
 ORDER BY TARIF ASC;
 -- Destinataire : Un client
 -- Interêt : Permet de trier le prix des hébergements en fonction du tarifs des chambres de manière croissante.
 
-SELECT CODE, NOMPROP, CAPACITE, TARIFBASECHAMBRE, CODECOMMUNE
-FROM HEBERGEMENT 
-WHERE CAPACITE < 4
-GROUP BY CODECOMMUNE
-HAVING TARIFBASECHAMBRE < 150;
+SELECT h.CODE, h.NOMPROP, h.CAPACITE, h.TARIFBASECHAMBRE, h.CODECOMMUNE, c.CODE
+FROM HEBERGEMENT h
+INNER JOIN COMMUNE c ON h.CODECOMMUNE = c.CODE
+GROUP BY h.CODECOMMUNE
+HAVING h.TARIFBASECHAMBRE < 150;
 -- Destinataire : Un client
 -- Interêt : Récupère les logement trier pour un budget limité et un nombre de personne minimale.
 
-SELECT CODE, CODEREGION, CODEEPI
-FROM HEBERGEMENT
-WHERE CODEEPI > 1
-GROUP BY CODEREGION NOT IN(SELECT h.code, h.TARIFCHAMBREBASE
-                           FROM HEBERGEMENT h
-                           WHERE TARIFCHAMBREBASE < 200);
+SELECT h.CODE, h.CODECOMMUNE, h.CODEEPI, c.CODE, e.CODE
+FROM HEBERGEMENT h
+INNER JOIN EPI e ON h.CODEEPI = e.CODE
+INNER JOIN COMMUNE c ON h.CODECOMMUNE = c.CODE
+WHERE h.CODEEPI > 1
+GROUP BY h.CODECOMMUNE NOT IN(SELECT h.code, h.TARIFCHAMBREBASE
+                              FROM HEBERGEMENT h
+                              WHERE TARIFCHAMBREBASE < 200);
 -- Destinataire : Un client
 -- Interêt : Tri un hébergement en fonction de plusieur critère sélectionner, l'épi doit être supérieur à
 -- 1, le prix inférieur à 200 et trier par région. 
