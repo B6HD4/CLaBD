517ebc011ddd454203d8e0aca110e4fff6cdf95f
Ced <crouille@iutbayonne.univ-pau.fr>
2026-01-12 19:08:02 +0100

    remove doublons

--- S1.04_groupe42_livrable3.sql
+++ S1.04_groupe42_livrable3.sql
@@ -45,11 +45,7 @@ ORDER BY TARIF ASC;
 
 -- GROUP BY --
 
-<<<<<<< HEAD
--- 5 : Nombre de chambres par hébergement
-=======
 -- 5 : Nombre de chambre par hébergement 
->>>>>>> 3605ff4 (Refactor SQL queries and comments in livrable3)
 SELECT h.code, COUNT(c.code) AS nbChambre
 FROM HEBERGEMENT h
 JOIN CHAMBRE c ON h.code=c.codeHeberg
@@ -58,11 +54,7 @@ GROUP BY h.code;
 -- Destinataire : Administrateur
 -- Intérêt : Utile pour faire des statistiques sur les hébergements
 
-<<<<<<< HEAD
--- 6 : Nombre d'hébergement par commune
-=======
 -- 6 : Nombre d'hébergements par commune
->>>>>>> 3605ff4 (Refactor SQL queries and comments in livrable3)
 SELECT c.code, COUNT(h.code) AS nbHebergements
 FROM COMMUNE c
 JOIN HEBERGEMENT h ON c.code=h.codeCommune
@@ -71,11 +63,7 @@ GROUP BY c.code;
 -- Destinataire : Administrateur
 -- Intérêt : Utile pour faire des statistiques sur les communes
 
-<<<<<<< HEAD
--- 7 : Nombre de commune par région
-=======
 -- 7 : Nombre de communes par région
->>>>>>> 3605ff4 (Refactor SQL queries and comments in livrable3)
 SELECT r.code, COUNT(c.code) AS nbCommune
 FROM REGION r
 JOIN COMMUNE c ON r.code=c.codeRegion
@@ -87,11 +75,7 @@ GROUP BY r.code;
 
 -- GROUP BY HAVING --
 
-<<<<<<< HEAD
--- 8 : Hebergements à moins de 50 euros
-=======
 -- 8 : Hébergement à moins de 50€
->>>>>>> 3605ff4 (Refactor SQL queries and comments in livrable3)
 SELECT code, MIN(tarifBaseChambre) AS "tarifMin" -- Y V E S
 FROM HEBERGEMENT
 GROUP BY code
@@ -125,11 +109,7 @@ HAVING AVG(h.TARIFBASECHAMBRE) < 150;
 
 -- Fontions d'agrégation --
 
-<<<<<<< HEAD
 -- 11 : Nombre de clients à Larrau
-=======
--- 11 : Nombres clients à larrau
->>>>>>> 3605ff4 (Refactor SQL queries and comments in livrable3)
 SELECT COUNT(DISTINCT c.code) AS "nbClients"
 FROM CLIENT c
 JOIN RESERVATION r ON c.code=r.codeClient
