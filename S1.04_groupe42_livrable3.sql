888ac8a3dbede55e753c59b688a749912b467434
B6HD4 <97739377+B6HD4@users.noreply.github.com>
2026-01-12 17:01:30 +0100

    Refactor SQL queries and comments for clarity

--- S1.04_groupe42_livrable3.sql
+++ S1.04_groupe42_livrable3.sql
@@ -5,7 +5,7 @@
 -- date de remise : 13/01/26                                                                            --
 ----------------------------------------------------------------------------------------------------------
 
--- ** 2 jointures (Ced)
+-- Jointures -- 
 
 SELECT R.nbrePers
 FROM RESERVATION R
@@ -22,26 +22,6 @@ JOIN CLIENT C ON C.code = R.CODECLIENT
 WHERE C.nom = 'Durand';
 -- Destinataire : 
 -- Interêt : Affiche les villes dans lesquelles a deja réservé le client Durand
-
--- ** 2 ORDER BY dont au moins 1 mettant en œuvre plusieurs attributs de tri (Thibaut)
-
--- ** 3 GROUP BY (criteres statiques) (Bixente)
-
--- ** 3 GROUP BY HAVING dont 1 avec sous-requête dans le HAVING (critere dynamique) (Bixente, Cédric, Thibaut)
-
-SELECT H.code, COUNT(C.code) AS nbDeChambres
-FROM HEBERGEMENT H
-JOIN CHAMBRE C ON C.codeHeberg = H.code
-GROUP BY H.code
-HAVING COUNT(C.code) >= (SELECT ROUND(AVG(R.NBREPERS))
-FROM RESERVATION R
-JOIN CLIENT C ON C.code = R.CODECLIENT
-WHERE C.nom = 'Durand');
--- lister les hebergements qui ont un nombre de chambres superieur ou egal au reservation deja passé au nom de durant.
-
--- ** Fonctions d’agrégation : 2 requêtes avec une ou plusieurs fonctions d’agrégation (MIN, MAX, etc.) (Bixente)
-
--- ** Sous-requêtes : 4 requêtes utilisant au niveau de l’imbrication des opérateurs de comparaison ainsi que les prédicats IN et NOT IN-- Jointures --
 
 
 -- ORDER BY --
@@ -76,7 +56,7 @@ GROUP BY r.code;
 
 -- GROUP BY HAVING --
 
-SELECT code, MIN(tarifBaseChambre) AS "tarifMin"
+SELECT code, MIN(tarifBaseChambre) AS "tarifMin" -- Y V E S
 FROM HEBERGEMENT
 GROUP BY code
 HAVING MIN(tarifBaseChambre) < 50;
@@ -84,6 +64,15 @@ HAVING MIN(tarifBaseChambre) < 50;
 -- Destinataire : Client
 -- Intérêt : Permet au client de savoir quells hébergements proposent des chambres à moins de 50€
 
+SELECT H.code, COUNT(C.code) AS nbDeChambres -- ced
+FROM HEBERGEMENT H
+JOIN CHAMBRE C ON C.codeHeberg = H.code
+GROUP BY H.code
+HAVING COUNT(C.code) >= (SELECT ROUND(AVG(R.NBREPERS))
+FROM RESERVATION R
+JOIN CLIENT C ON C.code = R.CODECLIENT
+WHERE C.nom = 'Durand');
+-- lister les hebergements qui ont un nombre de chambres superieur ou egal au reservation deja passé au nom de durant.
 
 -- Fontions d'agrégation --
 
@@ -113,3 +102,4 @@ WHERE codeCommune IN (SELECT codeCommune
 -- Récupère le code des hébergements dans la même ville que "M18"
 -- Destinataire : Client
 -- Intérêt : Si un hébergement n'a pas assez de place, permet à l'utilisateur de connaitre les autres hébergements de la même ville pour pouvoir y réserver des chambres
+
