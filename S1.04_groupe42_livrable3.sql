14d32a9f467f543e63905c9284339abdcf8b263e
ignir64 <thibaut210107@gmail.com>
2026-01-12 12:10:24 +0100

    Refactor SQL queries with comments and criteria changes
    Updated SQL queries to include comments and adjusted criteria for filtering accommodations.

--- S1.04_groupe42_livrable3.sql
+++ S1.04_groupe42_livrable3.sql
@@ -4,6 +4,7 @@ INNER JOIN EPI e ON h.CODEEPI = e.CODE
 ORDER BY e.CODE DESC;
 -- Destinataire : Un client
 -- Interêt : Permet de trier les hébergements de manière décroissante en fonction du nombre d'épis.
+-- OK
 
 
 SELECT CODE, TARIFBASECHAMBRE AS "TARIF", TARIFBASELITSUP,CAPACITE
@@ -11,24 +12,28 @@ FROM HEBERGEMENT
 ORDER BY TARIF ASC;
 -- Destinataire : Un client
 -- Interêt : Permet de trier le prix des hébergements en fonction du tarifs des chambres de manière croissante.
+-- OK
 
-SELECT h.CODE, h.NOMPROP, h.CAPACITE, h.TARIFBASECHAMBRE, h.CODECOMMUNE, c.CODE
+SELECT h.CODECOMMUNE, AVG(h.TARIFBASECHAMBRE) AS tarif_moyen
 FROM HEBERGEMENT h
 INNER JOIN COMMUNE c ON h.CODECOMMUNE = c.CODE
 GROUP BY h.CODECOMMUNE
-HAVING h.TARIFBASECHAMBRE < 150;
+HAVING AVG(h.TARIFBASECHAMBRE) < 150;
 -- Destinataire : Un client
 -- Interêt : Récupère les logement trier pour un budget limité et un nombre de personne minimale.
+-- OK
 
-SELECT h.CODE, h.CODECOMMUNE, h.CODEEPI, c.CODE, e.CODE
+SELECT h.CODE, h.CODECOMMUNE, h.CODEEPI, c.CODE AS CODE_COMMUNE, e.CODE AS CODE_EPI
 FROM HEBERGEMENT h
 INNER JOIN EPI e ON h.CODEEPI = e.CODE
 INNER JOIN COMMUNE c ON h.CODECOMMUNE = c.CODE
-WHERE h.CODEEPI > 1
-GROUP BY h.CODECOMMUNE NOT IN(SELECT h.code, h.TARIFCHAMBREBASE
-                              FROM HEBERGEMENT h
-                              WHERE TARIFCHAMBREBASE < 200);
+WHERE h.CODEEPI > 2
+AND h.CODE NOT IN (
+    SELECT h2.CODE
+    FROM HEBERGEMENT h2
+    WHERE h2.TARIFBASECHAMBRE > 200);
 -- Destinataire : Un client
 -- Interêt : Tri un hébergement en fonction de plusieur critère sélectionner, l'épi doit être supérieur à
 -- 1, le prix inférieur à 200 et trier par région. 
+-- OK
 
